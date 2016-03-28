c52e4b44112e8e505a1bd554de2175ef47787312
Ben.G <BJ-Send@yandex.ru>
2016-03-28 13:59:10 +0300

    New TestPattern

--- TV_Blast_TestPattern/TV_Blast_TestPattern.ino
+++ TV_Blast_TestPattern/TV_Blast_TestPattern.ino
@@ -9,35 +9,41 @@ int x,y;
 int fon = 100;
 int pix;
 
-const unsigned char trio [] = {
+const unsigned char trio [] = {   // Массив с картинкой (не реализовано)
 //
 };
 
 
 //////////////////////////////////////  ИНИЦИАЛИЗАЦИЯ  ///////////////////////////////////////
 void setup() {
-  Serial.begin(56000);
+  Serial.begin(9600);
 }
 //////////////////////////////////////  ОСНОВНОЙ ЦИКЛ  //////////////////////////////////////
 void loop()
 {
-  Serial.write(255);                     // Отправляем тестовый градиент
-  
-    for (int i = 0;i <pxls; i++)
-  {
-   // Serial.write(random(0, 250));
-   //  Serial.write(100);
-   //  Serial.write(trio[i]);
-   pix=fon;
-   pix = rect(i, 40, 80, 60, 100, 15, pix);
-   pix = rect(i, 120, 80, 140, 100, 15, pix);
-   pix = rect(i, 50, 20, 130, 40, 180, pix);
-   Serial.write(pix);
-  }
-//  j+=10;
+  Serial.write(255);                      // Отправляем тестовый градиент
+  TestPattern();                          // Рисуем "настроечнцю таблицу"
+  // Serial.write(random(0, 250));        // Белый шум 
+  // Serial.write(100);                   // Чистый экран одним цветом
+  // Serial.write(trio[i]);               // Массив с картинкой (не реализовано)
 delay(100);
 }
 /////////////////////////////////////////////////////////////////////////////////////////////
+void TestPattern(void)
+{
+    for (int i = 0;i <pxls; i++)
+  {
+   pix=fon;
+   pix = rect(i, 14, 14, 160, 130, 150, pix);
+   pix = rect(i, 50, 50, 123, 90, fon, pix);
+   
+   pix = rect(i, 24, 24, 40, 40, 50, pix);
+   pix = rect(i, 135, 24, 150, 40, 190, pix);
+   pix = rect(i, 24, 104, 40, 120, 0, pix);
+   pix = rect(i, 135, 104, 150, 120, 250, pix);
+   Serial.write(pix);
+  }
+}
 int rect(int i, int x1, int y1, int x2, int y2, int grey, int pix)
 {
     x=i/frameY;
